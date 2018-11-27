ebb8420421e6d2e9fccfd9f26fc9dde036d098ad
yadanar <khinyadanaraung@my.jcu.edu.au>
2018-11-27 13:29:22 +0800

    generate words

--- Prac_03/word_generator.py
+++ Prac_03/word_generator.py
@@ -2,33 +2,13 @@ import random
 
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
-def main():
-    word_format = input("Enter word format\n(v) - vowels\n(c) - consonants")
-    while not is_valid_format(word_format):
-        print("Invalid word format")
-        word_format = input("Enter word format\n(v) - vowels\n(c) - consonants")
 
-    word = ""
+word_format = "ccvcvvc"
+word = ""
 
-    for kind in word_format:
-        if kind == "c":
-            word += random.choice(CONSONANTS)
-        else:
-            word += random.choice(VOWELS)
-    print(word)
-
-def is_valid_format(word_format):
-    word_format = word_format.lower()
-    count = 0
-    is_valid = True
-    while is_valid and count < len(word_format):
-        if word_format[count] == "c" or word_format[count] == "v":
-            pass
-        else:
-            is_valid = False
-
-        count += 1
-
-    return is_valid
-
-main()
+for kind in word_format:
+    if kind == "c":
+        word += random.choice(CONSONANTS)
+    else:
+        word += random.choice(VOWELS)
+print(word)
